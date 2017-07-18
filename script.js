09c2be6ce89aa76c294ec95615856a743c2444bf
Adriel Arsenault <adriel.arsenault@gmail.com>
2017-07-18 13:41:47 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var a = 2 + 10 - 19 + 4 - 90 + 1;
+var b = -20 + -19 - (-10) - (-1) + 24;
+var c = (10/5) * 4 - 20;
+var d = 4096 % 12;
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+
